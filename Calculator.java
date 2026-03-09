13c633bc559f0ae034cf8c51496c9a9f2de0ee47
michaelzuluaga04 <michael.zuluaga@globant.com>
2026-03-09 08:52:10 -0500

    Ejercicio Calculadora 01

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+    
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Suma: " + calc.sum(5, 3));
+    }
+}
